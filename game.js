9d2abd002a9fa623b2d32618b5abdb9c3c091da7
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2023-06-21 23:47:11 +0530

    changes made

--- game.js
+++ game.js
@@ -2,25 +2,48 @@ var gamePattern = [];
 var buttonColours = ["red","blue","green","yellow"];
 var randomChosenColour;
 var userClickedPattern = [];
+var level=0;
+var gamestarted=false;
+
+$(document).keypress(function(){
+    if(gamestarted==false)
+    {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        gamestarted = true;
+    }
+});
+
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
 
 function nextSequence(){
-    var randomNumber=Math.random()*3;
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber=Math.random()*4;
     randomNumber = Math.floor(randomNumber);
     randomChosenColour = buttonColours[randomNumber];
-    
     gamePattern.push(randomChosenColour);
-
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
 
-$(".btn").on("click" , function(){
-    var userChosenColour = $this.attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-})
 
 function playSound(name){
     var audio = new Audio("sounds/"+name+".mp3");
-    playSound(audGio);
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+      }, 100);
 }
